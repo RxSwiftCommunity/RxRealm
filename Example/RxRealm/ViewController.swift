b39a6bc9a2bd76c0eb20d0145da46dfc13f34b4b
Marin Todorov <marin@underplot.com>
2016-04-20 00:15:21 +0200

    * added table view demo updated from realm results

--- Example/RxRealm/ViewController.swift
+++ Example/RxRealm/ViewController.swift
@@ -1,67 +1,55 @@
-//
-//  ViewController.swift
-//  RxRealm
-//
-//  Created by Marin Todorov on 04/19/2016.
-//  Copyright (c) 2016 Marin Todorov. All rights reserved.
-//
 
 import UIKit
 
 import RxSwift
+import RxCocoa
+
 import RealmSwift
 import RxRealm
 
-func delay(delay: NSTimeInterval, block: dispatch_block_t) {
-    let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC)))
-    dispatch_after(time, dispatch_get_main_queue(), block)
+//realm model
+class Lap: Object {
+    dynamic var time: NSTimeInterval = NSDate().timeIntervalSinceReferenceDate
 }
 
-class Dog: Object {}
-
-class Person: Object {
-    let dogs = List<Dog>()
-}
-
+//view controller
 class ViewController: UIViewController {
     let bag = DisposeBag()
+    let formatter: NSDateFormatter = {
+        let f = NSDateFormatter()
+        f.timeStyle = .LongStyle
+        return f
+    }()
+    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        //reset realm
         let realm = try! Realm()
         try! realm.write {
             realm.deleteAll()
         }
         
-        let person = Person(value: ["dogs": [Dog()]])
-        try! realm.write {
-            realm.add(person)
-        }
-        
-        person.dogs.asObservableArray()
-            .subscribeNext{list in
-                print("count as list: \(list.count)")
-        }.addDisposableTo(bag)
-        
-        
-        realm.objects(Person).asObservable()
-            .subscribeNext {result in
-                print("count as results: \(result.count)")
+        //results as observable
+        let laps = realm.objects(Lap).sorted("time", ascending: false).asObservable()
+
+        //bind to table
+        laps.bindTo(tableView.rx_itemsWithCellIdentifier("Cell", cellType: UITableViewCell.self)) {[weak self] row, element, cell in
+            cell.textLabel!.text = self!.formatter.stringFromDate(NSDate(timeIntervalSinceReferenceDate: element.time))
         }.addDisposableTo(bag)
 
-        realm.objects(Person).asObservableArray()
-            .subscribeNext {array in
-                print("count as array: \(array.count)")
-        }.addDisposableTo(bag)
-        
-        delay(1, block: {
-            let realm = try! Realm()
-            try! realm.write {
-                realm.add(Person())
-                realm.objects(Person).first!.dogs.appendContentsOf([Dog(), Dog()])
-            }
-        })
+        //start adding laps
+        addLap()
     }
     
+    func addLap() {
+        let realm = try! Realm()
+        try! realm.write {
+            realm.add(Lap())
+        }
+
+        performSelector(#selector(addLap), withObject: nil, afterDelay: Double(arc4random_uniform(3) + 2))
+    }
 }
