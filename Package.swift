121fdbb86996c5bf2f58317a7fd447fee3fcf1f0
Guilherme Ramos <guilherme.hor@gmail.com>
2019-05-07 22:51:21 -0300

    Updating dependencies

--- Package.swift
+++ Package.swift
@@ -3,13 +3,20 @@ import PackageDescription
 
 let package = Package(
     name: "RxRealm",
+    dependencies: [
+        .package(url: "https://github.com/AccioSupport/realm-cocoa.git", .branch("master"))
+    ]
     products: [
         .library(name: "RxRealm", targets: ["RxRealm"])
     ],
     targets: [
         .target(
             name: "RxRealm",
-            path: "Pod/Classes"
+            path: "Pod/Classes",
+            dependencies: [
+                "Realm",
+                "RealmSwift"
+            ]
         )
     ]
 )
