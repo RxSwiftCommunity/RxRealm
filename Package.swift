50accfcc2a58c68b8d9d9fad06a78d60671b88c8
Guilherme Ramos <guilherme.hor@gmail.com>
2019-05-07 23:09:59 -0300

    Fixing missing dependencies

--- Package.swift
+++ Package.swift
@@ -7,12 +7,15 @@ let package = Package(
         .library(name: "RxRealm", targets: ["RxRealm"])
     ],
     dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "4.0.0")),
         .package(url: "https://github.com/AccioSupport/realm-cocoa.git", .branch("master"))
     ],
     targets: [
         .target(
             name: "RxRealm",
             dependencies: [
+                    "RxSwift",
+                    "RxCocoa",
                     "Realm",
                     "RealmSwift"
                 ],
