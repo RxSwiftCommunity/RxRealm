bac74bdc0c04f5ef1c68d46021a115b536903366
Guilherme Ramos <guilherme.hor@gmail.com>
2019-07-23 12:44:27 -0300

    Add support for SPM based dependency managers (#126)

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "RxRealm",
+    products: [
+        .library(name: "RxRealm", targets: ["RxRealm"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0")),
+        .package(url: "https://github.com/realm/realm-cocoa.git", .upToNextMajor(from: "3.17.1"))
+    ],
+    targets: [
+        .target(
+            name: "RxRealm",
+            dependencies: [
+                    "RxSwift",
+                    "RxCocoa",
+                    "Realm",
+                    "RealmSwift"
+                ],
+            path: "Pod/Classes"
+        )
+    ]
+)
