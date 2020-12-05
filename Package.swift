7ce74c1861c29c07c427f93bae48705e2acc53d0
Ryne Cheow <rynecheow@gmail.com>
2020-12-05 23:10:54 +0800

    chore: Bump min target to v3

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(name: "RxRealm",
                       platforms: [
-                        .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
+                        .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v3)
                       ],
                       products: [
                         // Products define the executables and libraries produced by a package, and make them visible to other packages.
