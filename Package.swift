35ecf4f5c02bdff481cf5a79884f2f754601d421
Joseph Mattello <mail@joemattiello.com>
2024-05-07 23:25:11 -0400

    Update Package.swift for modern spm, dynamic lib
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

--- Package.swift
+++ Package.swift
@@ -1,41 +1,41 @@
-// swift-tools-version:5.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.7
 
 import PackageDescription
 
-let package = Package(name: "RxRealm",
-                      platforms: [
-                        .macOS(.v10_10), .iOS(.v11), .tvOS(.v9), .watchOS(.v3)
-                      ],
-                      products: [
-                        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-                        .library(name: "RxRealm",
-                                 targets: ["RxRealm"])
-                      ],
-
-                      dependencies: [
-                        // Dependencies declare other packages that this package depends on.
-                        .package(url: "https://github.com/realm/realm-swift.git", .upToNextMajor(from: "10.21.1")),
-                        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.5.0"))
-                      ],
-
-                      targets: [
-                        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-                        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-                        .target(name: "RxRealm",
-                                dependencies: [
-                                  .product(name: "RxSwift", package: "RxSwift"),
-                                  .product(name: "RealmSwift", package: "Realm"),
-                                  .product(name: "RxCocoa", package: "RxSwift")
-                                ],
-                                path: "Sources"),
-                        .testTarget(name: "RxRealmTests",
-                                    dependencies: [
-                                      .byName(name: "RxRealm"),
-                                      .product(name: "RxSwift", package: "RxSwift"),
-                                      .product(name: "RxBlocking", package: "RxSwift"),
-                                      .product(name: "RealmSwift", package: "Realm"),
-                                      .product(name: "RxCocoa", package: "RxSwift")
-                                    ])
-                      ],
-                      swiftLanguageVersions: [.v5])
+let package = Package(
+    name: "RxRealm",
+    platforms: [
+        .macOS(.v10_13), .iOS(.v12), .tvOS(.v12), .watchOS(.v4)
+    ],
+    products: [
+        .library(
+            name: "RxRealm",
+            type: .static,
+            targets: ["RxRealm"]
+        ),
+        .library(
+            name: "RxRealm-Dynamic",
+            type: .dynamic,
+            targets: ["RxRealm"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/realm/realm-swift.git", from: "10.50.0"),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.7.1")
+    ],
+    targets: [
+        .target(
+            name: "RxRealm",
+            dependencies: [
+                .product(name: "RealmSwift", package: "realm-swift"),
+                .product(name: "RxSwift", package: "RxSwift"),
+                .product(name: "RxCocoa", package: "RxSwift")
+            ],
+            path: "Sources"),
+        .testTarget(
+            name: "RxRealmTests",
+            dependencies: ["RxRealm"]
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
